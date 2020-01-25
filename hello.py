719aed4a35160a6ba3e819faea1ce60094470993
Fernando Soares <sax.soares@gmail.com>
2020-01-25 20:24:01 -0300

    Até 'Links' section

--- hello.py
+++ hello.py
@@ -1,9 +1,25 @@
 from flask import Flask
-from flask import request
+from flask import render_template
+from flask_bootstrap import Bootstrap
+
+# ...
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Seu navegador é {}</h1>'.format(user_agent)
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
